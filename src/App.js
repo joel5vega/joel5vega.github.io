e173ae6875d8e1d98651c7fc989dc336d85ac4fd
joel5vega <joel5vega@gmail.com>
2022-03-18 16:57:17 -0400

    stats added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Redes from './components/Redes';
+import Stats from './components/Stats';
+
 function App() {
   return (
     <div className="App">
-      
+
       <header className="App-header">
         <NavBar />
-        <Redes />
       </header>
+      <div className='container'>
+        <div >
+          <Redes />
+        </div>
+        <div >
+          <Stats />
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Stats.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Stats = () => {
+    return (
+        <div className="container">
+            Stats
+            <div className='tarjeta'><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=joel5vega&show_icons=true&locale=en&layout=compact" alt="joel5vega" /></div>
+            <div className='tarjeta'><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=joel5vega&layout=compact" alt="joel5vega" /></div>
+            <div className='tarjeta'><img align="center" src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=true&locale=en&layout=compact" alt="joel5vega" /></div>
+
+            
+
+        </div>
+    )
+}
+export default Stats;

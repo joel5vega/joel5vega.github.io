fa5cbdfce0950d63f165bf1578a6d0e65092121e
joel5vega <joel5vega@gmail.com>
2022-03-21 19:30:26 -0400

    cambio de iconos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,13 @@ const NavBar = () => {
 
         <Navbar sticky='top' collapseOnSelect defaultExpanded expand="lg" bg="dark" variant='dark'>
 
-            <Link to="/"><img className='icon' alt="home" src="https://cdn-icons-png.flaticon.com/512/619/619032.png" /></Link>
+            <Link to="/"><img className='icon' alt="home" src="home-blanco.png" /></Link>
             {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
             {/* <Navbar.Collapse id="responsive-navbar-nav"> */}
 
-            <Link className="enlace" to="/projects"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/1087/1087815.png" alt="projects" />Projects</Link>
-            <Link className="enlace" to="/stats"><img className='icon' src="https://cdn-icons-png.flaticon.com/512/921/921591.png" alt="stats" />Stats</Link>
-            <Link className="enlace" to="/aboutme"><img className='icon' alt="yo" src="https://cdn-icons-png.flaticon.com/512/4333/4333609.png" />Yo</Link>
+            <Link className="enlace" to="/projects"><img className='icon' src="proyecto-blanco.png" alt="projects" />Projects</Link>
+            <Link className="enlace" to="/stats"><img className='icon' src="stat-blanco.png" alt="stats" />Stats</Link>
+            <Link className="enlace" to="/aboutme"><img className='icon' alt="yo" src="persona-blanco.png" />Yo</Link>
 
 
             {/* </Navbar.Collapse> */}

7741a43c1a6ff84544b31268e02d06ea3bab8006
joel5vega <joel5vega@gmail.com>
2022-03-22 08:54:28 -0400

    arreglar proyecto

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,22 +7,12 @@ const NavBar = () => {
         <Navbar sticky='top' collapseOnSelect defaultExpanded expand="lg" bg="dark" variant='dark'>
             {/* <div className='opcion'> */}
                 <Link to="/"><img className='icon' alt="home" src="home-blanco.png" /></Link>
-            {/* </div> */}
-
-            {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-            {/* <Navbar.Collapse id="responsive-navbar-nav"> */}
-
             <Link className="enlace" to="/projects">
-                {/* <img className='icon' src="proyecto-blanco.png" alt="projects" /> */}
                 Projects</Link>
             <Link className="enlace" to="/stats">
-                {/* <img className='icon' src="stat-blanco.png" alt="stats" /> */}
                 Stats</Link>
             <Link className="enlace" to="/aboutme">
-                {/* <img className='icon' alt="yo" src="persona-blanco.png" /> */}
                 Profile</Link>
-
-
             {/* </Navbar.Collapse> */}
 
 
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -6,8 +6,7 @@ const Stats = () => {
             <div className="container">
             <div className='titulo'>Estadísticas </div>
                 <div className='tarjeta'><img  src="https://github-readme-stats.vercel.app/api/top-langs?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark" alt="joel5vega" /></div>
-                {/* <div className='tarjeta'><img src="https://github-readme-streak-stats.herokuapp.com/?user=joel5vega&layout=compact&theme=dark&hide=current" alt="joel5vega" /></div> */}
-                <div className='tarjeta'><img  src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark&hide=issues,prs" alt="joel5vega" /></div>
+                <div className='imagen'><img  src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark&hide=issues,prs" alt="joel5vega" /></div>
             </div>
 
 

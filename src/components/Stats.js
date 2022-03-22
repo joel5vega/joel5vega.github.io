b64ff76e3df56a093d11538b3b6d309b0409276b
joel5vega <joel5vega@gmail.com>
2022-03-22 09:06:25 -0400

    correcciones menores

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,9 +4,9 @@ const Stats = () => {
         <div >
 
             <div className="container">
-            <div className='titulo'>Estadísticas </div>
+            {/* <div className='titulo'>Estadísticas </div> */}
                 <div className='tarjeta'><img  src="https://github-readme-stats.vercel.app/api/top-langs?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark" alt="joel5vega" /></div>
-                <div className='imagen'><img  src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark&hide=issues,prs" alt="joel5vega" /></div>
+                <div className='imagen'><img  src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=false&locale=en&layout=compact&theme=dark&hide=issues,prs&hide_rank=true&custom_title=Github stats" alt="joel5vega" /></div>
             </div>
 
 

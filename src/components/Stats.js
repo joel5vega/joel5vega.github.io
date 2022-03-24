e248e00e2aa3e18fd95563bfec4ae9761d222433
joel5vega <joel5vega@gmail.com>
2022-03-24 00:53:45 -0400

    añadir api rescuetime

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,30 +1,43 @@
 import axios from 'axios';
 import React from 'react';
+// import SisHorarios from './SisHorarios';
 const rescuetimeURL = "https://www.rescuetime.com/anapi/daily_summary_feed?key=B632wU543wwlr1Aw96D0Jie8Xa806KxiKruYi9_4&format=json&interval=day"
 
 const Stats = () => {
 
     const [data, setData] = React.useState([]);
+    const CORS_SERVER = 'http://129.151.116.4:10000';
+
     React.useEffect(() => {
-        axios.defaults.headers.get['Content-Type'] ='application/json';
-        axios.get(rescuetimeURL)
+        axios.get(CORS_SERVER + '/' + rescuetimeURL)
             .then(res => {
                 setData(res.data);
                 console.log(res.data)
             })
     }, [])
-    if (!data) return null
 
     return (
         <div >
             <div className="container">
-                {/* <div className='titulo'>Estadísticas </div> */}
-                <div className="tarjeta"></div>
-                <div className='tarjeta'><img src="https://github-readme-stats.vercel.app/api/top-langs?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark" alt="joel5vega" /></div>
-                <div className='imagen'><img src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=false&locale=en&layout=compact&theme=dark&hide=issues,prs&hide_rank=true&custom_title=Github stats" alt="joel5vega" /></div>
+                <div className="box">
+                    {/* <SisHorarios data={data} /> */}
+                </div>
+                
+                <div className="box">
+                <div className='titulo-descripcion'>Datos obtenidos de la Api de Rescuetime</div>
+                    <table className='tabla'>
+                        <thead>
+                            <tr><td>Fecha</td><td>Productividad</td></tr>
+                        </thead>
+                        <tbody>
+                            {data.map(item => (<tr key={item.id}><td>{item.date}</td><td>{item.productivity_pulse}</td></tr>))}
+                        </tbody>
+
+                    </table>
+                </div>
+                <div className='box'><img src="https://github-readme-stats.vercel.app/api/top-langs?username=joel5vega&show_icons=true&locale=en&layout=compact&theme=dark" alt="joel5vega" /></div>
+                {/* <div className='box'><img src="https://github-readme-stats.vercel.app/api?username=joel5vega&show_icons=false&locale=en&layout=compact&theme=dark&hide=issues,prs,contributed&hide_rank=true&custom_title=Github stats" alt="joel5vega" /></div> */}
             </div>
-
-
         </div>
     )
 }

f3840f79cb3d9cea77b0bb4c6a62fe44cc58f41d
joel5vega <joel5vega@gmail.com>
2022-04-04 22:00:05 -0400

    data updated

--- src/components/stats/DataSumMonth.js
+++ src/components/stats/DataSumMonth.js
@@ -1,69 +1,69 @@
 const DataSumMonth=[
-    [
-      1,
-      254154,
-      1,
-      "Software Development"
-    ],
-    [
-      2,
-      220208,
-      1,
-      "Reference \u0026 Learning"
-    ],
-    [
-      3,
-      49280,
-      1,
-      "Business"
-    ],
-    [
-      4,
-      28853,
-      1,
-      "Design \u0026 Composition"
-    ],
-    [
-      5,
-      23835,
-      1,
-      "Social Networking"
-    ],
-    [
-      6,
-      21518,
-      1,
-      "Utilities"
-    ],
-    [
-      7,
-      12828,
-      1,
-      "Uncategorized"
-    ],
-    [
-      8,
-      9239,
-      1,
-      "Communication \u0026 Scheduling"
-    ],
-    [
-      9,
-      6248,
-      1,
-      "Entertainment"
-    ],
-    [
-      10,
-      2962,
-      1,
-      "News \u0026 Opinion"
-    ],
-    [
-      11,
-      203,
-      1,
-      "Shopping"
-    ]
+  [
+    1,
+    308277,
+    1,
+    "Software Development"
+  ],
+  [
+    2,
+    269691,
+    1,
+    "Reference \u0026 Learning"
+  ],
+  [
+    3,
+    58268,
+    1,
+    "Business"
+  ],
+  [
+    4,
+    43635,
+    1,
+    "Design \u0026 Composition"
+  ],
+  [
+    5,
+    31732,
+    1,
+    "Social Networking"
+  ],
+  [
+    6,
+    25048,
+    1,
+    "Utilities"
+  ],
+  [
+    7,
+    14386,
+    1,
+    "Uncategorized"
+  ],
+  [
+    8,
+    10172,
+    1,
+    "Communication \u0026 Scheduling"
+  ],
+  [
+    9,
+    7325,
+    1,
+    "Entertainment"
+  ],
+  [
+    10,
+    3092,
+    1,
+    "News \u0026 Opinion"
+  ],
+  [
+    11,
+    217,
+    1,
+    "Shopping"
   ]
+]
 export default DataSumMonth;

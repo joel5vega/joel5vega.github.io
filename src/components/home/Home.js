93dd091cab79a7fb7347196d0cc83b58597645aa
joel5vega <joel5vega@gmail.com>
2022-04-19 20:54:01 -0400

    actualizar

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,18 +7,21 @@ import Quote from '../../components/home/Quote';
 
 const Home = () => {
     return (
-         <section>
+        <section>
             <div className='container header__container'>
                 <img className='foto-perfil' src={Perfil} alt="Joel Vega" />
                 <h5>Hello, I'm.</h5>
                 <h1>Joel Vega</h1>
                 <h5 className="text-light>">I'm a IT Ops Engineer & Linux Administrator </h5>
                 <div className='tarjeta'> <img src="https://komarev.com/ghpvc/?username=joel5vega&label=Profile%20views&color=0e75b6&style=flat" alt="joel5vega" /> </div>
-                <Quote/>
+                <Quote />
                 <CTA />
+                <div className='badges'>
+                    <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="HORIZONTAL" data-vanity="joel5vega" data-version="v1"></div>
+                </div>
                 <Redes />
             </div>
-         </section>
+        </section>
     )
 
 }
--- src/components/stats/DataSummary.js
+++ src/components/stats/DataSummary.js
@@ -1,45 +1,69 @@
 const DataSummary = [
-    [
-      1,
-      5851,
-      1,
-      "Software Development"
-    ],
-    [
-      2,
-      1800,
-      1,
-      "Utilities"
-    ],
-    [
-      3,
-      289,
-      1,
-      "Business"
-    ],
-    [
-      4,
-      241,
-      1,
-      "Uncategorized"
-    ],
-    [
-      5,
-      56,
-      1,
-      "Reference \u0026 Learning"
-    ],
-    [
-      6,
-      20,
-      1,
-      "Entertainment"
-    ],
-    [
-      7,
-      4,
-      1,
-      "Design \u0026 Composition"
-    ]
+  [
+    1,
+    152017,
+    1,
+    "Reference \u0026 Learning"
+  ],
+  [
+    2,
+    88009,
+    1,
+    "Software Development"
+  ],
+  [
+    3,
+    35626,
+    1,
+    "Design \u0026 Composition"
+  ],
+  [
+    4,
+    23779,
+    1,
+    "Utilities"
+  ],
+  [
+    5,
+    13107,
+    1,
+    "Social Networking"
+  ],
+  [
+    6,
+    12517,
+    1,
+    "Business"
+  ],
+  [
+    7,
+    10867,
+    1,
+    "Uncategorized"
+  ],
+  [
+    8,
+    8994,
+    1,
+    "Communication \u0026 Scheduling"
+  ],
+  [
+    9,
+    4750,
+    1,
+    "Entertainment"
+  ],
+  [
+    10,
+    662,
+    1,
+    "News \u0026 Opinion"
+  ],
+  [
+    11,
+    114,
+    1,
+    "Shopping"
   ]
+]
 export default DataSummary;

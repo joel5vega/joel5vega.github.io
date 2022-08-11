abb294af43c474d2ebeb5cff54b569798f1bd77a
Joel Vega <joel5vega@gmail.com>
2022-08-11 11:37:26 -0400

    Updates

---
+++ static/js/570.86342d2c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[570],{1570:function(e,o,l){l.r(o);l(2791);var r=l(184);o.default=function(){return(0,r.jsx)("img",{src:"https://komarev.com/ghpvc/?username=joel5vega&label=Profile%20views&color=4db5ff",alt:"joel5vega"})}}}]);
+//# sourceMappingURL=570.86342d2c.chunk.js.map

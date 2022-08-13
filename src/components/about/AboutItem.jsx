894d0f71553cd929c3bf9d3061de9e5569bb63be
Joel Vega <joel5vega@gmail.com>
2022-08-13 09:36:14 -0400

    fix about

--- src/components/about/AboutItem.jsx
+++ src/components/about/AboutItem.jsx
@@ -1,26 +1,26 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 
 //Fotos
-import {FcExpand} from "react-icons/fc";
+import { FcExpand } from "react-icons/fc";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest
+  })
 }));
 
 export default function RecipeReviewCard(props) {
@@ -31,40 +31,28 @@ export default function RecipeReviewCard(props) {
   };
 
   return (
-    <Card sx={{ maxWidth: 345, background:"var(--color-bg)"}}>
-      <CardHeader sx={{color:"var(--color-primary)"}}
+    <Card sx={{  background: "var(--color-bg)" }}>
+      <CardHeader
+        sx={{ color: "var(--color-primary)" }}
         avatar={
-          <Avatar sx={{ bgcolor: "var(--color-bg)" }} aria-label="recipe">
+          <Avatar sx={{ bgcolor: "var(--color-bg)" }} aria-label="item">
             {props.logo}
           </Avatar>
         }
         action={
-          <IconButton aria-label="settings">
-            {/* <MoreVertIcon /> */}
+          <IconButton onClick={handleExpandClick} aria-label="settings">
+            <FcExpand />
           </IconButton>
         }
         title={<h2>{props.title}</h2>}
         // subheader="September 14, 2016"
       />
-    
-     {props.photo}
-      <CardActions disableSpacing>
-       
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-        <FcExpand/>
-        </ExpandMore>
-      </CardActions>
+
+      {props.photo}
+
       <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent  sx={{color:"var(--color-light)"}}>
-          <Typography paragraph>
-           {props.description}
-          </Typography>
-          
+        <CardContent sx={{ color: "var(--color-light)" }}>
+          <Typography paragraph>{props.description}</Typography>
         </CardContent>
       </Collapse>
     </Card>

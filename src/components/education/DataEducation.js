12f3a000f6f8155d2987de29f1db7b819b8c8958
joel5vega <joel5vega@gmail.com>
2023-06-15 21:46:35 -0400

    updated data

--- src/components/education/DataEducation.js
+++ src/components/education/DataEducation.js
@@ -16,42 +16,36 @@ const DataEducation = {
         }
     ],
     "Cloud": [
-        { "id": 6, "name": "AWS", "icon": "aws", "link": "https://drive.google.com/file/d/1t38Uxw3imU04qYufqbeRi9l760ihBKjF/preview" },
+        { "id": 6, "name": "AWS", "icon": "aws", "link": "https://drive.google.com/file/d/1OGJCye2NeuoAZzS0zAzW1lA9KOj85WDR/preview" },
         {
-            "id": 30, "name": "Azure", "icon": "azure", "link": "https://drive.google.com/file/d/1BDiacHUciFhrFnmq1eYqsCnyvQ9H_u5Y/preview",
+            "id": 30, "name": "Azure", "icon": "azure", "link": "https://drive.google.com/file/d/1pOpI-xekD0VDKuPlKAtXeWU3SF_U9WDc/preview",
         },
         {
-            "id": 8,
-            "name": "OCI", "icon": "oci",
-        }, {
             "id": 7,
-            "name": "Google Cloud", "icon": "gcp", "description": "Google Cloud Fundamentals on Coursera", "link": "https://drive.google.com/file/d/15flvsweA9MuK9YQOa_y5iMw_cD1gtZrK/preview"
+            "name": "Google Cloud", "icon": "gcp", "description": "Google Cloud Fundamentals on Coursera", "link": "https://drive.google.com/file/d/1L7q0EMYs5jCEOrc00y2vbCS76JFrrEID/preview"
         },
 
     ],
     "Infra": [
-        {"id": 9,"name": "Docker", "icon": "docker", "link": "https://drive.google.com/file/d/1dK64_PyPMMZAermSFjpWg7QnCSEOB_cZ/preview"}, 
-        { "id": 12, "name": "Sophos", "icon": "sophos", "link": "https://sophos.netexam.com/certs/11017/32007D9A4DED4879B350BFEF5A1BB86E131432.pdf" },
-        { "id": 13, "name": "CCNA", "icon": "cisco", "link": "https://drive.google.com/file/d/18h8udjhUUM13fzjBwSnrxBP0JgrdH4iJ/preview" }, 
-        { "id": 14, "name": "MySQL", "icon": "mysql", "link": "https://drive.google.com/file/d/1J0mYolwnih9m16ah-TFa0Xy5N-jXFzkk/preview" },
-        { "id": 21, "name": "Containers", "icon":"container","link": "https://drive.google.com/file/d/1jRXvOe8wlgvd5eQa-xKBGKNbZq_vHYv6/preview" },
-        { "id": 16, "name": "Telecom", "icon": "telecom", "link": "https://drive.google.com/file/d/17GoxFKHlbbnEWhV605zqaQq_vZ6gfjlP/preview" },
-        
-        { "id": 10, "name": "Kubernetes", "icon": "kubernetes", "link":"https://drive.google.com/file/d/1Cib4RwoRFptVLSWNHUdzI5yYwSpiU2my/preview"}, 
+        {"id": 9,"name": "Docker", "icon": "docker", "link": "https://drive.google.com/file/d/1Q1-lQUGY4L3Z1ls95RW3zAa9NugexPYQ/preview"}, 
+        { "id": 12, "name": "Sophos", "icon": "sophos", "link": "https://drive.google.com/file/d/1D5qJGfNthEiVYE2M8dWXnnAojNdvE_LY/preview" },
+        { "id": 13, "name": "CCNA", "icon": "cisco", "link": "https://drive.google.com/file/d/15I_L_2OKp8zZsz07Dfhk6_XD4YabXkDt/preview" }, 
+        { "id": 14, "name": "MySQL", "icon": "mysql", "link": "https://drive.google.com/file/d/1K4LPTn219IJZqNK_SqMAa5vmB-sqiRJQ/preview" },
+        { "id": 21, "name": "Containers", "icon":"container","link": "https://drive.google.com/file/d/1eB4o4VJ3NFqif0GGeyZtKwkl4DVdlEwD/preview" },
+        { "id": 10, "name": "Kubernetes", "icon": "kubernetes", "link":"https://drive.google.com/file/d/1j5nsmAbB0DfNkCFkZDxsp3JRU1EbWGKI/preview"}, 
         {"id": 11, "name": "Ansible", "icon": "ansible",},
-        { "id": 15, "name": "Rancher", "icon": "rancher" },
-        { "id": 24, "name": "DevOps", "icon": "devops","link":"https://drive.google.com/file/d/1q-fHjVdt_TrcNKfhMOEmsPNV_1Y-reR2/preview" },
-        { "id": 25, "name": "Gitlab", "icon": "gitlab", "link": "https://drive.google.com/file/d/1tagW3TVa1R3u6c4fjJA2onbf5R6vWRaa/preview" },
+        { "id": 24, "name": "DevOps", "icon": "devops","link":"https://drive.google.com/file/d/1xoyuqQzB2v1ipwoVm1HN-5aMqeJzQVQP/preview" },
+        { "id": 25, "name": "Gitlab", "icon": "gitlab", "link": "https://drive.google.com/file/d/1UmdLHpUIAS1Ioe98mM3VtBz5nr45jSKq/preview" },
 
     ],
     "Code": [
-        { "id": 17, "name": "Python", "icon": "python", "link": "https://drive.google.com/file/d/1-PwRa10MkAPWmAkXqJTsSfXoydmqxAyK/preview" }, { "id": 18, "name": "PHP", "icon": "php" }, { "id": 19, "name": "JavaScript", "icon": "js" },
-        { "id": 20, "name": "Bash", "icon": "bash" }, { "id": 22, "name": "Software Development", "icon": "development", "link": "https://drive.google.com/file/d/1cVkm2_WCyZGPKnoLTrOdKwa_RS9U0TWe/preview", },
+        { "id": 17, "name": "Python", "icon": "python", "link": "https://drive.google.com/file/d/1tH0gGsVjJRkK5koOrx2FWdy64mENoYIA/preview" }, { "id": 18, "name": "PHP", "icon": "php" }, { "id": 19, "name": "JavaScript", "icon": "js" },
+        { "id": 20, "name": "Bash", "icon": "bash" }, { "id": 22, "name": "Software Development", "icon": "development", "link": "https://drive.google.com/file/d/16HMd22NLQxg4KaI4slDIlZszU0BCbeLX/preview", },
         { "id": 23, "name": "React", "icon": "react" }
     ],
     "Lang": [
-        { "id": 50, "name": "English", "icon": "english", "level": "Advanced", "link": "https://drive.google.com/file/d/1bOmSIRUIDlNBhKvI5klTDP085pVfw7jb/preview" },
-        { "id": 51, "name": "Chinese", "icon": "chinese", "level": "Basic", "link": "https://drive.google.com/file/d/17GpPWiHd4cOkikQ7jCuXwuwBGeFxQ7Oc/preview" },
+        { "id": 50, "name": "English", "icon": "english", "level": "Advanced", "link": "https://drive.google.com/file/d/1f0QB_hJf9F2CRLFklt98XmwkjXa6-KbP/preview" },
+        { "id": 51, "name": "Chinese", "icon": "chinese", "level": "Basic", "link": "https://drive.google.com/file/d/1HfKSULqHle6bBwoSaCl3R3Wf7YDby88x/preview" },
         { "id": 52, "name": "Spanish", "icon": "spanish", "level": "Native Speaker" },
 
 
